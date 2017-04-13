0457070d3c5b91d9ad28d4877d46b44ff9c93292
minyounglee <mlee17@stanford.edu>
2017-04-13 11:27:41 -0700

    deleted param B

--- pdFitCG.m
+++ pdFitCG.m
@@ -103,10 +103,9 @@ function [fit, fieldNames] = pdFitCG(sbjID)
 		mu = fit{i}.fitparams(1);
 		sigma = fit{i}.fitparams(2);
 		lambda = fit{i}.fitparams(3);
-		B = fit{i}.fitparams(4);
 		cgfunc = fit{i}.cgfunc;
-		fitVal = arrayfun(@(x) cgfunc(mu,sigma,lambda,B,x), xs);
-		pfitVal = arrayfun(@(x) cgfunc(mu,sigma,lambda,B,x), testVal);
+		fitVal = arrayfun(@(x) cgfunc(mu,sigma,lambda,x), xs);
+		pfitVal = arrayfun(@(x) cgfunc(mu,sigma,lambda,x), testVal);
 		error = sqrt(pfitVal.*(1-pfitVal)./nTotal);
 		if i < 7
 			subplot(nrow/2,ncol,i)
@@ -117,14 +116,14 @@ function [fit, fieldNames] = pdFitCG(sbjID)
 		hold on;
     	plot(xs, fitVal, 'k')
     	if taskType==1
-    		title(sprintf('Stair Contrast: Ecc=%d   (N=%d) \n \\mu=%0.2f \\sigma=%0.2f \\lambda=%0.2f B=%0.2f \n', ...
-    			stimStr(1).eccentricity, nTrialCombined, mu,sigma,lambda,B), 'Interpreter','tex');
+    		title(sprintf('Stair Contrast: Ecc=%d   (N=%d) \n \\mu=%0.2f \\sigma=%0.2f \\lambda=%0.2f \n', ...
+    			stimStr(1).eccentricity, nTrialCombined, mu,sigma,lambda), 'Interpreter','tex');
     		ylabel('Percent Correct');
     		xlabel('Contrast');
     		axis([0 0.6 0 1]); box off; drawnow;
     	else
-    		title(sprintf('Ecc=%d   Con=%0.3f   (N=%d) \n \\mu=%0.2f \\sigma=%0.2f \\lambda=%0.2f B=%0.2f \n', ...
-    			stimStr(1).eccentricity, stimStr(1).stimulus.contrast, nTrialCombined, mu,sigma,lambda,B), 'Interpreter','tex');
+    		title(sprintf('Ecc=%d   Con=%0.3f   (N=%d) \n \\mu=%0.2f \\sigma=%0.2f \\lambda=%0.2f \n', ...
+    			stimStr(1).eccentricity, stimStr(1).stimulus.contrast, nTrialCombined, mu,sigma,lambda), 'Interpreter','tex');
     		ylabel('Percent Choice Interval 1');
     		xlabel('Postition difference of targets between interval 1 and 2 (deg)');
     		axis([-6 6 0 1]); box off; drawnow;
